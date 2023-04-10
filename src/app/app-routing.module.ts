148be2c151cae970224a51fdc6b7be723752372a
AbhishekArora-accolite <abhishek.arora@accolitedigital.com>
2023-04-10 11:48:30 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ApplicationsComponent } from './applications/applications.component';
+
+const routes: Routes = [
+  {
+    path:'applications',
+    component:ApplicationsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
